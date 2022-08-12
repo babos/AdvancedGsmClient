c6e237a63c84d30d1057b3d842b63d4d96774d0d
Sly Gryphon <sgryphon@computer.org>
2022-08-12 11:08:33 +1000

    Basic sending

--- examples/HttpClient/src/main.cpp
+++ examples/HttpClient/src/main.cpp
@@ -1,19 +1,37 @@
 #define GSM_BAUDRATE 115200
+
+// Set serial for output console (to the Serial Monitor, default speed 115200)
+#define SerialMon Serial
+
+// Set serial for AT commands (to the module)
+#define SerialAT Serial1
+
+// Set serial for debug, if wanted
 #define ADVANCED_GSM_DEBUG Serial;
+
+// See all AT commands, if wanted
+#define DUMP_AT_COMMANDS
 
 #include "../../../src/SIM7020/GsmModemSIM7020.h"
 
 #include <Arduino.h>
 
 // Allocate memory for concrete object
-GsmModemSIM7020 sim7020(Serial1);
-// But access it via the abstract interface
+#ifdef DUMP_AT_COMMANDS
+#include <StreamDebugger.h>
+StreamDebugger debugger(SerialAT, SerialMon);
+GsmModemSIM7020 sim7020(debugger);
+#else
+GsmModemSIM7020 sim7020(SerialAT);
+#endif
+
+// Access via the API
 GsmModem& modem = sim7020;
 
 void setup() {
-  Serial.begin(115200);
+  SerialMon.begin(115200);
   delay(5000);
-  Serial.print("GsmHttpClient\n(with Arduino framework)\nvia PlatformIO\n");
+  SerialMon.print("GsmHttpClient\n(with Arduino framework)\nvia PlatformIO\n");
 
   modem.begin();
   //modem.test();
