0f2f1fe787b842b94a4904838e3c710a6c8bd8e6
Sly Gryphon <sgryphon@computer.org>
2022-08-27 15:16:07 +1000

    Handle both password and no password

--- src/SIM7020/SIM7020MqttClient.cpp
+++ src/SIM7020/SIM7020MqttClient.cpp
@@ -37,9 +37,14 @@ int16_t SIM7020MqttClient::connect(const char client_id[],
 
   // Connect
   // TODO: Should store client_id as field?
-  this->modem.sendAT(GF("+CMQCON="), this->mqtt_id, ',', mqtt_version, ",\"",
-                    client_id, "\",", mqtt_keep_alive_s, ',', clean_session,
-                    ",0,", user_name, ",", password);
+  this->modem.stream.printf(GF("AT+CMQCON=%d,%d,\"%s\",%d,%d"),
+                            this->mqtt_id, mqtt_version, client_id, mqtt_keep_alive_s, clean_session);
+  this->modem.stream.print(GF(",0")); // Will
+  if (user_name != nullptr) {
+    this->modem.stream.printf(GF(",%s,%s"), user_name, password);
+  }
+  this->modem.stream.print(GSM_NL);
+
   rc = this->modem.waitResponse(60000);
   if (rc == 0) {
     ADVGSM_LOG(GsmSeverity::Error, "SIM7200",
